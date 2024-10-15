f2624704171885a08e15aa5d9bdd21cdf82a7d0e
koni <152221158+konikonikoni@users.noreply.github.com>
2024-10-15 18:16:39 +0200

    Update index.php Update style.css Add folder.svg - Buttons for Login/Sign Up - folder.svg as img

--- index.php
+++ index.php
@@ -14,53 +14,28 @@ $request = trim($_SERVER['REQUEST_URI'], '/');
 <body>
     <main>
         <div id="svg">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100">
-                <!-- Folder Shape -->
-                <rect x="10" y="30" width="80" height="50" fill="none" stroke="black" stroke-width="2"/>
-
-                <!-- Folder Tab -->
-                <rect x="10" y="20" width="30" height="10" fill="none" stroke="black" stroke-width="2"/>
-
-                <!-- Zipper -->
-                <rect x="50.5" y="30" width="2" height="50" fill="black"/>
-                <rect x="55" y="32.5" width="5" height="2.5" fill="black"/>
-                <rect x="46" y="35" width="5" height="2.5" fill="black"/>
-                <rect x="55" y="37.5" width="5" height="2.5" fill="black"/>
-                <rect x="46" y="40" width="5" height="2.5" fill="black"/>
-                <rect x="55" y="42.5" width="5" height="2.5" fill="black"/>
-                <rect x="46" y="45" width="5" height="2.5" fill="black"/>
-                <rect x="55" y="47.5" width="5" height="2.5" fill="black"/>
-                <rect x="46" y="50" width="5" height="2.5" fill="black"/>
-                <rect x="55" y="52.5" width="5" height="2.5" fill="black"/>
-                <rect x="46" y="55" width="5" height="2.5" fill="black"/>
-                <rect x="55" y="57.5" width="5" height="2.5" fill="black"/>
-                <rect x="46" y="60" width="5" height="2.5" fill="black"/>
-                <rect x="55" y="62.5" width="5" height="2.5" fill="black"/>
-                <rect x="46" y="65" width="5" height="2.5" fill="black"/>
-                <rect x="55" y="67.5" width="5" height="2.5" fill="black"/>
-                <rect x="46" y="70" width="5" height="2.5" fill="black"/>
-                <rect x="55" y="72.5" width="5" height="2.5" fill="black"/>
-                <rect x="46" y="75" width="5" height="2.5" fill="black"/>
-            </svg>
+            <img src="/images/folder.svg" alt="zip-Folder">
         </div>
         <?php if ($request === 'register'): ?>
             <div id="register">
-                <form>
+                <form method="POST">
                     <input type="text" placeholder="username" id="username" name="username">
                     <input type="password" placeholder="password" id="password" name="password">
-                    <input type="password" placeholder="password" id="password" name="password">
+                    <input type="password" placeholder="retype password" id="retype_password" name="retype_password">
                     <div id="submit">
-                        <input type="submit" value="Sign Up">
+                        <input type="submit" value="Sign up">
+                        <input type="button" value="Already signed up" onclick="location.href='/'">
                     </div>
                 </form>
             </div>
         <?php else: ?>
             <div id="login">
-                <form>
+                <form method="POST">
                     <input type="text" placeholder="username" id="username" name="username">
                     <input type="password" placeholder="password" id="password" name="password">
                     <div id="submit">
                         <input type="submit" value="Log In">
+                        <input type="button" value="Sign up" onclick="location.href='/register'">
                     </div>
                 </form>
             </div>
